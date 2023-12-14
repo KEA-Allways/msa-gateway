67bfc8946e06740b9be1062207681f92ba6248d1
“mj” <75360915+minjun0707@users.noreply.github.com>
2023-12-14 14:52:01 +0900

    Jenkins Build

--- src/main/java/com/allways/TokenHelper.java
+++ src/main/java/com/allways/TokenHelper.java
@@ -8,6 +8,7 @@ public class TokenHelper {
 	private final JwtHandler jwtHandler;
 	private final String key;
 	private final long maxAgeSeconds;
+	
 
 	public String createToken(String subject){
 		return jwtHandler.createToken(key, subject, maxAgeSeconds);
